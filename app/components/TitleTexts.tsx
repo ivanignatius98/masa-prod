b16616ef99e8e408136d1716719426618b2a6e87
Ivan Handoko <ivanignatius98@gmail.com>
2024-02-01 17:11:26 +0700

    add title texts animation

---
+++ app/components/TitleTexts.tsx
@@ -0,0 +1,48 @@
+import { motion } from "framer-motion";
+
+export const TitleTexts = () => {
+  const container = {
+    visible: {
+      transition: {
+        delayChildren: 0.1, // Delay before stagger starts
+        staggerChildren: 0.4, // Stagger duration between children
+      },
+    }
+  }
+
+  const item = {
+    hidden: {
+      height: "0",
+    },
+    visible: {
+      height: "auto",
+    }
+  }
+  const menuItems: string[] = [
+    "WE BUILD",
+    "DIGITAL",
+    "FLAGSHIP",
+    "STORES",
+  ]
+  return (
+    <div className="flex flex-col justify-end h-72 px-8">
+      <div className="overflow-hidden">
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={container}
+          className=" uppercase text-black font-semibold text-7xl"
+        >
+          {menuItems.map((row, i) => (
+            <motion.h1 key={i}
+              variants={item}
+            >
+              {row}
+            </motion.h1>
+          ))}
+        </motion.div>
+      </div>
+    </div>
+  )
+}
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,39 @@
 "use client";
 import "./styles.css";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import { AnimatePresence } from "framer-motion";
 import { MenuToggle } from "@components/MenuToggle";
 import { Menu } from "@components/Menu";
+import { motion } from "framer-motion";
+import { TitleTexts } from "@components/TitleTexts";
 
 export default function App() {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div>
+    <Fragment>
+      <div className="flex flex-1 w-full">
+        <motion.div
+          className="flex-1 bg-white text-black"
+          variants={{
+            hidden: {
+              opacity: 0,
+            },
+            visible: {
+              opacity: 1,
+              transition: {
+                delay: 1.7
+              }
+            }
+          }}
+          initial="hidden"
+          animate="visible"
+        >
+        </motion.div>
+        <div className="flex items-center flex-1">
+          <TitleTexts />
+        </div>
+      </div>
+
       {/* Sidebar */}
       <AnimatePresence
         initial={false}
@@ -18,8 +43,6 @@ export default function App() {
       </AnimatePresence>
       <MenuToggle isOpen={isOpen} toggle={() => setIsOpen(!isOpen)} />
       {/* Sidebar End */}
-
-
-    </div>
+    </Fragment >
   );
 }
