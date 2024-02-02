74453223258c4cb09b93b8712e724d62cd2d934d
Ivan Handoko <ivanignatius98@gmail.com>
2024-02-02 11:27:23 +0700

    wip - disable scrolling

--- app/components/HomeSectionLeft.tsx
+++ app/components/HomeSectionLeft.tsx
@@ -1,26 +1,26 @@
 "use client";
 import { motion } from "framer-motion";
-import Image from 'next/image'
+import Image from "next/image";
 import { useCallback, useEffect, useState } from "react";
 
 const Page = () => {
-  const [animateScroll, setAnimateScroll] = useState(false)
+  const [animateScroll, setAnimateScroll] = useState(false);
   const [offset, setOffset] = useState(0);
-  useEffect(() => {
-    if (animateScroll) {
-      const interval = setInterval(() => setOffset((prev) => prev + 1), 10);
-      return () => {
-        clearInterval(interval);
-      };
-    }
-  }, [animateScroll]);
+  // useEffect(() => {
+  //   if (animateScroll) {
+  //     const interval = setInterval(() => setOffset((prev) => prev + 0.5), 10);
+  //     return () => {
+  //       clearInterval(interval);
+  //     };
+  //   }
+  // }, [animateScroll]);
   useEffect(() => {
     const timeoutId = setTimeout(() => setAnimateScroll(true), 4000); // 1000 milliseconds (1 second)
     return () => clearTimeout(timeoutId);
   }, []);
   return (
-    <motion.div
-      className="flex-1 bg-white hidden md:block pr-8 overflow-hidden"
+    <motion.main
+      className="flex-1 bg-white hidden lg:block pr-8 overflow-hidden"
       variants={{
         hidden: {
           opacity: 0,
@@ -28,29 +28,37 @@ const Page = () => {
         visible: {
           opacity: 1,
           transition: {
-            delay: 1.7
-          }
-        }
+            delay: 1.7,
+          },
+        },
       }}
       initial="hidden"
       animate="visible"
     >
       <div className="flex gap-8">
-        <div className="transition-all w-full " style={{ transform: `translateY(-${offset / 2}px)` }}>
-          {[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,].map((v, i) =>
-            <div key={i} className="mb-8 inline-block">
-              <Image
-                className="aspect-[3/4] object-cover"
-                src={"https://placehold.co/800x1200/orange/white.png"}
-                alt="Picture of the author"
-                width={400}
-                height={600}
-              />
-            </div>
-          )}
+        <div className="w-full">
+          <div
+            className="transition-all"
+            style={{ transform: `translateY(-${offset}px)` }}
+          >
+            {[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map((v, i) => (
+              <div key={i} className="mb-8 inline-block">
+                <Image
+                  className="aspect-[3/4] object-cover"
+                  src={"https://placehold.co/800x1200/orange/white.png"}
+                  alt="Picture of the author"
+                  width={400}
+                  height={600}
+                />
+              </div>
+            ))}
+          </div>
         </div>
-        <div className="transition-all w-full " style={{ transform: `translateY(-${offset / 2}px)` }}>
-          {[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,].map((v, i) =>
+        <div
+          className="transition-all w-full"
+          style={{ transform: `translateY(-${offset}px)` }}
+        >
+          {[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map((v, i) => (
             <div key={i} className="mb-8 inline-block">
               <Image
                 className="aspect-[2/3] object-cover"
@@ -60,12 +68,11 @@ const Page = () => {
                 height={600}
               />
             </div>
-          )}
-
+          ))}
         </div>
       </div>
-    </motion.div>
-  )
+    </motion.main>
+  );
 };
 
 export default Page;
