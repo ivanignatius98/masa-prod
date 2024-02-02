e8d583dd5b68817665607df5df6a79ec35930c83
Ivan Handoko <ivanignatius98@gmail.com>
2024-02-02 19:47:47 +0700

    add handle scroll

--- app/components/HomeSectionLeft.tsx
+++ app/components/HomeSectionLeft.tsx
@@ -18,6 +18,17 @@ const Page = () => {
     const timeoutId = setTimeout(() => setAnimateScroll(true), 4000); // 1000 milliseconds (1 second)
     return () => clearTimeout(timeoutId);
   }, []);
+
+  const [scrollDirection, setScrollDirection] = useState("");
+
+  const handleScroll = (event: any) => {
+    // Detect the scroll direction
+    const direction = event.deltaY > 0 ? "down" : "up";
+    console.log(event.deltaY);
+    setOffset((prev) => prev + event.deltaY)
+    // Update the scroll direction state
+    setScrollDirection(direction);
+  };
   return (
     <motion.main
       className="flex-1 bg-white hidden lg:block pr-8 overflow-hidden"
@@ -35,11 +46,15 @@ const Page = () => {
       initial="hidden"
       animate="visible"
     >
-      <div className="flex gap-8">
+      <div
+        className="flex gap-8"
+        onWheel={handleScroll}
+        onTouchMove={handleScroll}
+      >
         <div className="w-full">
           <div
             className="transition-all"
-            style={{ transform: `translateY(-${offset}px)` }}
+            style={{ transform: `translateY(-${offset}px) translateZ(0px)` }}
           >
             {[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map((v, i) => (
               <div key={i} className="mb-8 inline-block">
@@ -56,7 +71,7 @@ const Page = () => {
         </div>
         <div
           className="transition-all w-full"
-          style={{ transform: `translateY(-${offset}px)` }}
+          style={{ transform: `translateY(-${offset}px)  translateZ(0px)` }}
         >
           {[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map((v, i) => (
             <div key={i} className="mb-8 inline-block">
